929d444668d5ecfd814b744b1e8ff01da85ece19
psyciknz <github@andc.co.nz>
2022-12-31 10:00:57 +1300

    Added TV function, added movies and tv templates

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ def influx_health():
     return "<p>MongoDB health check: {}</p>".format(status)
 #def influx_health():
 
-@app.route("/watch")
-def influx_watch():
+@app.route("/movies")
+def influx_watch_movies():
     host=os.getenv('INFLUX_HOST')
     port=os.getenv('INFLUX_PORT')
     username=os.getenv('INFLUX_USER')
@@ -54,8 +54,51 @@ def influx_watch():
     cli = InfluxDBClient(host=host,port=port,username=username,
                 password=password,database=database)
 
-    result = cli.query("select time,title,tmdb_id, url,poster_html from watch where time > now() - 30d and type = 'movie'")
-    return render_template("movies.html", movies=result)
+    tabledata = cli.query("select time,title,tmdb_id, url,poster_html from watch \
+        where time > now() - 30d and type = 'movie' \
+            order by time desc")
+    data_points = []
+    for measurement, tags in tabledata.keys():
+        for p in tabledata.get_points(measurement=measurement, tags=tags):
+            data_points.append(p)
+
+    #records = tabledata.get_points('watch')
+    #movies=list(records)
+    #for record in movies:
+    #    print(record[0])
+    #records = result.items()[0]
+    return render_template("movies_bootstrap.html",title="Movies", measurement="watch",
+        columns=["time","title","tmdb_id","url","poster_html"],
+        points=data_points)
+#def influx_watch():
+
+@app.route("/tv")
+def influx_watch_tv():
+    host=os.getenv('INFLUX_HOST')
+    port=os.getenv('INFLUX_PORT')
+    username=os.getenv('INFLUX_USER')
+    password=os.getenv('INFLUX_PWD')
+    database=os.getenv('INFLUX_DB')
+
+    cli = InfluxDBClient(host=host,port=port,username=username,
+                password=password,database=database)
+
+    tabledata = cli.query("select time,/show/,season, episode,title,episode_url,url \
+            from watch where time > now() - 30d and type = 'episode' \
+                order by time desc")
+    data_points = []
+    for measurement, tags in tabledata.keys():
+        for p in tabledata.get_points(measurement=measurement, tags=tags):
+            data_points.append(p)
+
+    #records = tabledata.get_points('watch')
+    #movies=list(records)
+    #for record in movies:
+    #    print(record[0])
+    #records = result.items()[0]
+    return render_template("tv_bootstrap.html",title="TV", measurement="watch",
+        columns=["time","show","season","episode","title","episode_url","url"],
+        points=data_points)
 #def influx_watch():
     
 
